4650b7b956a377de807aae440fb8b4143e4e4c56
ramya <ramya@DESKTOP-CMNA655.Dlink>
2024-07-09 13:26:31 +0530

    commit message

---
+++ corejavaprogram/src/com/tnsif/encapsulation/Demo.java
@@ -0,0 +1,13 @@
+package com.tnsif.encapsulation;
+
+public class Demo {
+	public static void main(String[]args) {
+		Encapsulation e=new Encapsulation();
+		e.serialnumber=1;
+		e.name="navya";
+		e.age=20;
+		
+		e.show();
+	}
+
+}
