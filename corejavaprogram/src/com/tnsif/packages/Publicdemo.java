e4bdf7ece754dffdb77e8bcdb1b870fe1ca6a56e
ramya <ramya@DESKTOP-CMNA655.Dlink>
2024-07-09 13:35:09 +0530

    commit message

---
+++ corejavaprogram/src/com/tnsif/packages/Publicdemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.packages;
+
+	public class Publicdemo {
+		
+		int a=10;
+		public static void main(String[]args) {
+			Publicdemo p=new Publicdemo();
+			System.out.println(p.a);
+		}
+	}
+
